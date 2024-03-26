5f1c7cefab23a744e11186280f4a898fff6009de
MOLLY-S1 <98923584+MOLLY-S1@users.noreply.github.com>
2024-03-27 09:57:13 +1300

    Update

---
+++ Exercise_1.py
@@ -0,0 +1,9 @@
+from tkinter import *
+root = Tk()
+
+root.title("Welcome!")
+welcome = Label(root, bg="Black", fg="White", text="Welcome!", font=("Times", 50, "bold"))
+
+welcome.pack()
+
+root.mainloop()
