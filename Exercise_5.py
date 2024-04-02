af165707740806777ad55e930f014d510b4d5714
MOLLY-S1 <98923584+MOLLY-S1@users.noreply.github.com>
2024-04-02 17:54:36 +1300

    Update Exercise_5.py

--- Exercise_5.py
+++ Exercise_5.py
@@ -2,12 +2,13 @@ from tkinter import *
 root = Tk()
 
 root.title("Testing Screen")
+root.minsize(600, 400)
 
-red = Label(root, bg="Red", fg="White", text="red", font=("Ariel", 40, "bold"))
+red = Label(root, bg="Red", fg="White", text="Red", font=("Ariel", 40, "bold"))
 blue = Label(root, bg="Blue", fg="White", text="Blue", font=("Ariel", 40, "bold"))
 green = Label(root, bg="Green", fg="White", text="Green", font=("Ariel", 40, "bold"))
 
-red.pack(expand=YES)
-blue.pack(fill=X, side=BOTTOM)
-green
+red.pack(side=RIGHT, fill=BOTH, padx= 30, pady=30, expand=YES)
+blue.pack(side=LEFT, fill=Y)
+green.pack(side=BOTTOM, fill=X)
 root.mainloop()
